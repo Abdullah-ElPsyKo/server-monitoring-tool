06e26b16231469415983f2ad339130a248fa8a4d
Abdulla Bagishev <abdullahbagishev@icloud.com>
2023-11-05 16:39:44 +0100

    ping hosts with command-line arguments

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import sys, os, csv, time
+from checker import ping_server
+
+
 def main():
-    pass
+    if len(sys.argv) < 2:
+        print("Use: python main.py [manage | check]")
+        return
+
+    if sys.argv[1] == "manage":
+        pass
+
+    elif sys.argv[1] == "check":
+        print("What check do you want to do?")
+        print("1 - Ping")
+
+        check = input("Check: ")
+
+        if check == "1":
+            host = input("Host: ")
+            status = ping_server(host)
+
+    else:
+        print("Use: python main.py [manage | check]")
+        return
 
 
 if __name__ == "__main__":
