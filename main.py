989727e407ac6a09a063fed8c8ed373d4afbc439
Abdulla Bagishev <abdullahbagishev@icloud.com>
2023-11-05 16:40:21 +0100

    create csv file to add result of ping

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ def main():
         print("Use: python main.py [manage | check]")
         return
 
+    if os.path.isfile("status_check.csv") is False:  # Check if status file exists
+        print("status_check.csv not found, creating...")
+        f = open("status_check.csv", "w")
+        f.write("host,status,time\n")
+        f.close()
+
     if sys.argv[1] == "manage":
         pass
 
@@ -19,6 +25,10 @@ def main():
         if check == "1":
             host = input("Host: ")
             status = ping_server(host)
+
+            with open("status_check.csv", "a", newline="") as file:  # Append status to csv file
+                writer = csv.writer(file)
+                writer.writerow([host, status,time.strftime("%Y-%m-%d %H:%M:%S")])
 
     else:
         print("Use: python main.py [manage | check]")
