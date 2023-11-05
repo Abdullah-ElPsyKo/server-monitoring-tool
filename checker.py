83186d6c3b971207739a663a8bff5782ffbca3f3
Abdulla Bagishev <abdullahbagishev@icloud.com>
2023-11-05 15:50:07 +0100

    create ping function

--- checker.py
+++ checker.py
@@ -1,3 +1,10 @@
+from ping3 import ping
 
-def ping_server():
-    pass
+
+def ping_server(host):
+    response = ping(host)
+
+    if response is False:
+        return False
+    else:
+        return True
