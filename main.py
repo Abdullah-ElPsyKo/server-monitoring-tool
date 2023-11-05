4c81d606acbc0592bcc0cf677a26f9c7c11a55b6
Abdulla Bagishev <abdullahbagishev@icloud.com>
2023-11-05 17:56:44 +0100

    add while loop for ping checks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys, os, csv, time
 from checker import ping_server
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+from jinja2 import Environment, FileSystemLoader
 
 
 def main():
@@ -18,21 +18,22 @@ def main():
         pass
 
     elif sys.argv[1] == "check":
-        print("What check do you want to do?")
-        print("1 - Ping")
+        check = ""
+        while check != "2":
+            print("What check do you want to do?")
+            print("1 - Ping\n2 - Quit")
+            check = input("Check: ")
 
-        check = input("Check: ")
+            if check == "1":
+                host = input("Host: ")
+                status = ping_server(host)
 
-        if check == "1":
-            host = input("Host: ")
-            status = ping_server(host)
-
-            with open("status_check.csv", "a", newline="") as file:  # Append status to csv file
-                writer = csv.writer(file)
-                writer.writerow([host, status,time.strftime("%Y-%m-%d %H:%M:%S")])
-            
-            data = read_csv_to_dict("status_check")
-            render_template("check", data, "check")
+                with open("status_check.csv", "a", newline="") as file:  # Append status to csv file
+                    writer = csv.writer(file)
+                    writer.writerow([host, status,time.strftime("%Y-%m-%d %H:%M:%S")])
+                
+                data = read_csv_to_dict("status_check")
+                render_template("check", data, "check")
             
     else:
         print("Use: python main.py [manage | check]")
